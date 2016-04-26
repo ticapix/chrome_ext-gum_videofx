3c81713a1daa0606617a12343e98f295c2d92827
ticapix <ticapix@gmail.com>
2016-04-26 23:47:23 +0300

    simplify code

--- ext/js/fx_3dcube.js
+++ ext/js/fx_3dcube.js
@@ -1,46 +1,35 @@
-// three.js cube drawing
-function fx_3dcube(video) {
-    var scene = new THREE.Scene();
-    // var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var camera = new THREE.PerspectiveCamera(75, 640 / 480, 0.1, 1000);
-    // load a texture, set wrap mode to repeat
-    var texture = new THREE.Texture(video);
-    // texture.wrapS = THREE.RepeatWrapping;
-    // texture.wrapT = THREE.RepeatWrapping;
-    texture.wrapS = THREE.ClampToEdgeWrapping;
-    texture.wrapT = THREE.ClampToEdgeWrapping;
-    texture.minFilter = THREE.NearestFilter;
-    texture.repeat.set(1, 1);
-    var geometry = new THREE.BoxGeometry(3, 3, 3);
-    var material = new THREE.MeshLambertMaterial({
-        map: texture
-    });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 4;
-    var light = new THREE.AmbientLight('rgb(255,255,255)'); // soft white light
-    scene.add(light);
-    // White directional light at half intensity shining from the top.
-    //var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-    //directionalLight.position.set( 0, 1, 0 );
-    //scene.add( directionalLight );
-    // white spotlight shining from the side, casting shadow
-    var spotLight = new THREE.SpotLight('rgb(255,255,255)');
-    spotLight.position.set(100, 1000, 1000);
-    spotLight.castShadow = true;
-    spotLight.shadow.mapSize.width = 1024;
-    spotLight.shadow.mapSize.height = 1024;
-    spotLight.shadow.camera.near = 500;
-    spotLight.shadow.camera.far = 4000;
-    spotLight.shadow.camera.fov = 30;
-    scene.add(spotLight);
-    //render the scene
-    function render() {
-        requestAnimationFrame(render);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        texture.needsUpdate = true;
-        window.pipeline_renderer.render(scene, camera);
+// https://github.com/chrisdavidmills/threejs-video-cube
+define_module('fx_3dcube', function() {
+    var self = {}
+        // three.js cube drawing
+    self.main = function(video) {
+        var scene = new THREE.Scene();
+        debug('video', video)
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10);
+        // var camera = new THREE.PerspectiveCamera(75, video.videoWidth / video.videoHeight, 0.1, 1000);
+        // load a texture, set wrap mode to repeat
+        var texture = new THREE.Texture(video);
+        texture.minFilter = THREE.NearestFilter;
+        var geometry = new THREE.BoxGeometry(3, 3, 3);
+        var material = new THREE.MeshLambertMaterial({
+            map: texture
+        });
+        var cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        camera.position.z = 4;
+        var light = new THREE.AmbientLight('rgb(255,255,255)'); // soft white light
+        scene.add(light);
+        //render the scene
+        var render = function() {
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            if (video.readyState === video.HAVE_ENOUGH_DATA) {
+                texture.needsUpdate = true;
+            }
+            window.pipeline_renderer.render(scene, camera);
+            requestAnimationFrame(render);
+        }
+        render();
     }
-    render();
-}
+    return self;
+}());
