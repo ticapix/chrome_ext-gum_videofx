e5d0e90c4f1786bee54c3e3b7c62fe9c6bef67d2
ticapix <ticapix@gmail.com>
2016-04-27 22:49:20 +0300

    uses reveal module pattern

--- ext/js/app.js
+++ ext/js/app.js
@@ -1,91 +1,14 @@
 var app_init = (function() {
     debug('creating app ' + application_name)
-    var self = {}
-    self.StateEnum = {
+    var StateEnum = {
         UNINSTALLED: 0,
         LAZY_INSTALLED: 1,
         INSTALLED: 2
     }
-    self.status = self.StateEnum.UNINSTALLED;
-    self.videofx = {}
-    self.installGumLazyHook = function() {
-        'use strict';
-        window.MediaStream = window.webkitMediaStream;
-        window.navigator.__getUserMedia = Navigator.prototype.webkitGetUserMedia;
-        Navigator.prototype.webkitGetUserMedia = function(constraints, onSuccess, onFail) {
-            debug('GetUserMedia hook called')
-            var _onSuccess = null
-            new Promise(function(resolve, reject) {
-                switch (self.status) {
-                    case self.StateEnum.LAZY_INSTALLED:
-                        self.sendMessage2ContentScript({
-                            video_effect_init: true
-                        }).then(function(data) {
-                            if (data.done === true) {
-                                self.status = self.StateEnum.INSTALLED;
-                                resolve();
-                            }
-                        })
-                        break;
-                    case self.StateEnum.INSTALLED:
-                        resolve();
-                        break;
-                    default:
-                        reject();
-                }
-            }).then(function() {
-                navigator.__getUserMedia(constraints, function(stream) {
-                    self.createNewStream(stream).then(function(stream) {
-                        onSuccess(stream);
-                    })
-                }, onFail)
-            });
-        }
-        self.status = self.StateEnum.LAZY_INSTALLED
-    }
-    self.installDomElt = function() {
-        window.pipeline_renderer = new THREE.WebGLRenderer();
-        window.pipeline_renderer.setSize(640, 480);
-        window.pipeline_renderer.domElement.setAttribute('style', 'width: 1px;height: 1px;');
-        (document.body || document.getElementsByTagName('head')[0] || document.documentElement).appendChild(window.pipeline_renderer.domElement);
-        self.pipeline_video = document.createElement("video");
-    }
-    self.createNewStream = function(stream) {
-        window.stream_orign = stream;
-        if (window.stream_orign.getVideoTracks().length > 0) {
-            debug('stream origin', window.stream_orign.getTracks())
-            debug('self.pipeline_video', self.pipeline_video)
-            return new Promise(function(resolve, reject) {
-                var handler = function() {
-                    debug('oncanplay')
-                    self.pipeline_video.play();
-                    threeRender(self.pipeline_video);
-                    var stream_from_effect = window.pipeline_renderer.domElement.captureStream(30);
-                    for (var audio_track of window.stream_orign.getAudioTracks()) {
-                        stream_from_effect.addTrack(audio_track);
-                    }
-                    stream_from_effect.getTracks().forEach(function(track) {
-                        track.addEventListener('stop', function() {
-                            debug('stop', this, this, window.stream_orign.getTracks())
-                        })
-                    });
-                    debug('stream from effect', stream_from_effect.getTracks())
-                    self.pipeline_video.removeEventListener('canplay', handler, false)
-                    resolve(stream_from_effect);
-                }
-                self.pipeline_video.addEventListener('canplay', handler);
-                // TODO: maybe taking the 1st video track is not optimal
-                var stream_to_effect = new MediaStream([window.stream_orign.getVideoTracks()[0]]);
-                debug('stream to effect', stream_to_effect.getTracks());
-                self.pipeline_video.src = window.URL.createObjectURL(stream_to_effect);
-            })
-        } else {
-            return new Promise(function(resolve, reject) {
-                resolve(window.stream_orign);
-            })
-        }
-    }
-    self.sendMessage2ContentScript = function(data) {
+    var _status = StateEnum.UNINSTALLED;
+    var _videofx = {}
+    var _pipeline_video = document.createElement("video");
+    var _sendMessage2ContentScript = function(data) {
         // https://gist.github.com/gordonbrander/2230317
         var ID = function() {
             // Math.random should be unique because of its seeding algorithm.
@@ -108,9 +31,95 @@ var app_init = (function() {
         document.dispatchEvent(evt);
         return promise;
     }
-    self.defineModule = function(name, module) {
-        debug('loading videofx', name)
-        self.videofx[name] = module;
+    var _threeRender = function(video) {
+        var fx = 'fx_null'
+        fx = 'fx_3dcube'
+        return _videofx[fx].main(video);
     }
-    return self;
+    var _createNewStream = function(stream) {
+        window.stream_orign = stream;
+        if (window.stream_orign.getVideoTracks().length > 0) {
+            debug('stream origin', window.stream_orign.getTracks())
+            debug('_pipeline_video', _pipeline_video)
+            return new Promise(function(resolve, reject) {
+                var handler = function() {
+                    debug('oncanplay', _pipeline_video)
+                    _pipeline_video.play();
+                    _threeRender(_pipeline_video);
+                    var stream_from_effect = window.pipeline_renderer.domElement.captureStream(30);
+                    for (var audio_track of window.stream_orign.getAudioTracks()) {
+                        stream_from_effect.addTrack(audio_track);
+                    }
+                    stream_from_effect.getTracks().forEach(function(track) {
+                        track.addEventListener('stop', function() {
+                            debug('stop', this, this, window.stream_orign.getTracks())
+                        })
+                    });
+                    debug('stream from effect', stream_from_effect.getTracks())
+                    _pipeline_video.removeEventListener('canplay', handler, false)
+                    resolve(stream_from_effect);
+                }
+                _pipeline_video.addEventListener('canplay', handler);
+                // TODO: maybe taking the 1st video track is not optimal
+                var stream_to_effect = new MediaStream([window.stream_orign.getVideoTracks()[0]]);
+                debug('stream to effect', stream_to_effect.getTracks());
+                _pipeline_video.src = window.URL.createObjectURL(stream_to_effect);
+            })
+        } else {
+            return new Promise(function(resolve, reject) {
+                resolve(window.stream_orign);
+            })
+        }
+    }
+    var installGumLazyHook = function() {
+        'use strict';
+        window.MediaStream = window.webkitMediaStream;
+        window.navigator.__getUserMedia = Navigator.prototype.webkitGetUserMedia;
+        Navigator.prototype.webkitGetUserMedia = function(constraints, onSuccess, onFail) {
+            debug('GetUserMedia hook called')
+            var _onSuccess = null
+            new Promise(function(resolve, reject) {
+                switch (_status) {
+                    case StateEnum.LAZY_INSTALLED:
+                        _sendMessage2ContentScript({
+                            video_effect_init: true
+                        }).then(function(data) {
+                            if (data.done === true) {
+                                _status = StateEnum.INSTALLED;
+                                resolve();
+                            }
+                        })
+                        break;
+                    case StateEnum.INSTALLED:
+                        resolve();
+                        break;
+                    default:
+                        reject();
+                }
+            }).then(function() {
+                navigator.__getUserMedia(constraints, function(stream) {
+                    _createNewStream(stream).then(function(stream) {
+                        onSuccess(stream);
+                    })
+                }, onFail)
+            });
+        }
+        _status = StateEnum.LAZY_INSTALLED
+    }
+    var installDomElt = function() {
+        window.pipeline_renderer = new THREE.WebGLRenderer();
+        window.pipeline_renderer.setSize(640, 480);
+        window.pipeline_renderer.domElement.setAttribute('style', 'width: 1px;height: 1px;');
+        (document.body || document.getElementsByTagName('head')[0] || document.documentElement).appendChild(window.pipeline_renderer.domElement);
+    }
+    var defineModule = function(name, module) {
+        debug('loading videofx', name)
+        _videofx[name] = module;
+    }
+    return {
+        installGumLazyHook: installGumLazyHook,
+        installDomElt: installDomElt,
+        defineModule: defineModule,
+        videofx: _videofx
+    };
 });
