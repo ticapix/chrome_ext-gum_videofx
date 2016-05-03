61ac1a315d124e4dc8103be18fb30caf9bb60345
ticapix <ticapix@gmail.com>
2016-05-03 23:06:13 +0300

    be a bit more jshint friendly

--- ext/js/app.js
+++ ext/js/app.js
@@ -1,13 +1,14 @@
+'use strict';
 var app_init = (function() {
-    debug('creating app ' + application_name)
+    debug('creating app ' + application_name);
     var StateEnum = {
         UNINSTALLED: 0,
         LAZY_INSTALLED: 1,
         INSTALLED: 2,
         RUNNING: 3
-    }
+    };
     var _status = StateEnum.UNINSTALLED;
-    var _videofx = {}
+    var _videofx = {};
     var _pipeline_video = document.createElement("video");
     var _pipeline_renderer = null;
     var _sendMessage2ContentScript = function(data) {
@@ -18,82 +19,80 @@ var app_init = (function() {
             // after the decimal.
             return '_' + Math.random().toString(36).substr(2, 9);
         };
-        var detail = data || {}
-        detail._id = ID()
+        var detail = data || {};
+        detail._id = ID();
         var promise = new Promise(function(resolve, reject) {
             var handler = function(evt) {
-                document.removeEventListener(detail._id, handler, false)
+                document.removeEventListener(detail._id, handler, false);
                 resolve(evt.detail);
-            }
-            document.addEventListener(detail._id, handler)
-        })
+            };
+            document.addEventListener(detail._id, handler);
+        });
         var evt = new CustomEvent("web2cs", {
             detail: detail
         });
         document.dispatchEvent(evt);
         return promise;
-    }
+    };
     var _threeRender = function(video) {
-        var fx = 'fx_null'
-        fx = 'fx_3dcube'
+        var fx = 'fx_null';
+        fx = 'fx_3dcube';
         return _videofx[fx].main(video);
-    }
+    };
     var _render = function(scene, camera) {
         _pipeline_renderer.render(scene, camera);
-        debug(_status === StateEnum.RUNNING)
+        debug(_status === StateEnum.RUNNING);
         return _status === StateEnum.RUNNING;
-    }
+    };
     var _createNewStream = function(stream) {
         if (stream.getVideoTracks().length > 0) {
-            debug('_pipeline_video', _pipeline_video)
+            debug('_pipeline_video', _pipeline_video);
             return new Promise(function(resolve, reject) {
                 var handler = function() {
-                    debug('video canplay')
+                    debug('video canplay');
                     _pipeline_video.play();
                     if (_status !== StateEnum.INSTALLED) {
-                        reject('webfx is not in INSTALLED state')
+                        reject('webfx is not in INSTALLED state');
                     }
                     _status = StateEnum.RUNNING;
                     _threeRender(_pipeline_video);
                     var stream_from_effect = _pipeline_renderer.domElement.captureStream(30);
                     if (stream_from_effect.getTracks().length != 1) {
-                        reject('too many tracks (>1) returned from captureStream(30)')
+                        reject('too many tracks (>1) returned from captureStream(30)');
                     }
                     stream_from_effect.getVideoTracks()[0].addEventListener('ended', function(evt) {
                         if (evt.target instanceof CanvasCaptureMediaStreamTrack) {
-                            debug('stopping video capture')
+                            debug('stopping video capture');
                             _status = StateEnum.INSTALLED;
                             // if canvas ended, ends the original video stream too
-                            stream_video_orign.stop()
+                            stream_video_orign.stop();
                         }
-                    })
+                    });
                     for (var audio_track of stream.getAudioTracks()) {
                         stream_from_effect.addTrack(audio_track);
                     }
                     debug('stream from effect', stream_from_effect.getTracks());
                     _pipeline_video.removeEventListener('canplay', handler, false);
                     resolve(stream_from_effect);
-                }
+                };
                 _pipeline_video.addEventListener('canplay', handler);
                 // TODO: maybe taking the 1st video track is not optimal
                 var stream_video_orign = stream.getVideoTracks()[0];
                 var stream_to_effect = new MediaStream([stream_video_orign]);
                 debug('stream to effect', stream_to_effect.getTracks());
                 _pipeline_video.src = window.URL.createObjectURL(stream_to_effect);
-            })
+            });
         } else {
             return new Promise(function(resolve, reject) {
                 resolve(stream);
-            })
+            });
         }
-    }
+    };
     var _installGumLazyHook = function() {
-        'use strict';
         window.MediaStream = window.webkitMediaStream;
         window.navigator.__getUserMedia = Navigator.prototype.webkitGetUserMedia;
         Navigator.prototype.webkitGetUserMedia = function(constraints, onSuccess, onFail) {
-            debug('GetUserMedia hook called')
-            var _onSuccess = null
+            debug('GetUserMedia hook called');
             new Promise(function(resolve, reject) {
                 switch (_status) {
                     case StateEnum.LAZY_INSTALLED:
@@ -104,7 +103,7 @@ var app_init = (function() {
                                 _status = StateEnum.INSTALLED;
                                 resolve();
                             }
-                        })
+                        });
                         break;
                     case StateEnum.INSTALLED:
                         resolve();
@@ -116,32 +115,32 @@ var app_init = (function() {
                 navigator.__getUserMedia(constraints, function(stream) {
                     _createNewStream(stream).then(function(stream) {
                         onSuccess(stream);
-                    })
-                }, onFail)
+                    });
+                }, onFail);
             });
-        }
-        _status = StateEnum.LAZY_INSTALLED
-    }
+        };
+        _status = StateEnum.LAZY_INSTALLED;
+    };
     var _installDomElt = function() {
         _pipeline_renderer = new THREE.WebGLRenderer();
         _pipeline_renderer.setSize(640, 480);
         _pipeline_renderer.domElement.setAttribute('style', 'width: 1px;height: 1px;');
         (document.body || document.getElementsByTagName('head')[0] || document.documentElement).appendChild(_pipeline_renderer.domElement);
-    }
+    };
     var _defineModule = function(name, module) {
-        debug('loading videofx', name)
+        debug('loading videofx', name);
         _videofx[name] = module;
-    }
+    };
     var self = {
-            installGumLazyHook: _installGumLazyHook,
-            installDomElt: _installDomElt,
-            defineModule: _defineModule,
-            render: _render
-        }
-        // Object.defineProperty(self, "pipeline_renderer", {
-        //     get: function() {
-        //         return _pipeline_renderer;
-        //     }
-        // })
+        installGumLazyHook: _installGumLazyHook,
+        installDomElt: _installDomElt,
+        defineModule: _defineModule,
+        render: _render
+    };
+    // Object.defineProperty(self, "pipeline_renderer", {
+    //     get: function() {
+    //         return _pipeline_renderer;
+    //     }
+    // })
     return self;
 });
--- ext/js/main.js
+++ ext/js/main.js
@@ -1,15 +1,16 @@
+'use strict';
 // https://cdn.rawgit.com/uysalere/js-demos/master/intro.html
 function check_browser_feature_support() {
     if (window.webkitMediaStream === undefined) {
-        debug('MediaStream not supported')
+        debug('MediaStream not supported');
         return false;
     }
     if (Navigator.prototype.webkitGetUserMedia === undefined) {
-        debug('GetUserMedia not supported')
+        debug('GetUserMedia not supported');
         return false;
     }
     if (document.createElement('canvas').captureStream === undefined) {
-        debug('HTMLCanvasElement.captureStream not supported. Try enabling flag #enable-experimental-web-platform-features in chrome://flags/')
+        debug('HTMLCanvasElement.captureStream not supported. Try enabling flag #enable-experimental-web-platform-features in chrome://flags/');
         return false;
     }
     return true;
@@ -26,23 +27,23 @@ document.addEventListener('web2cs', function(evt) {
     if (evt.detail.video_effect_init === true) {
         // GetUserMedia got called.
         // need to load our code for the video effect
-        inject_code("javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()")
+        inject_code("javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()");
         inject_script(chrome.extension.getURL('lib/three.min.js')).then(function() {
-            return inject_code('window[application_name].installDomElt()')
+            return inject_code('window[application_name].installDomElt()');
         }).then(function() {
             return new Promise(function(resolve, reject) {
                 chrome.runtime.sendMessage({
                     get_plugin_scripts: true
                 }, function(videofx) {
-                    var fx_promises = []
+                    var fx_promises = [];
                     for (var fx of videofx) {
-                        fx_promises.push(inject_file(fx))
+                        fx_promises.push(inject_file(fx));
                     }
                     return Promise.all(fx_promises).then(function() {
-                        resolve()
-                    })
-                })
-            })
+                        resolve();
+                    });
+                });
+            });
         }).then(function() {
             responseEvent(evt, {
                 done: true
@@ -61,18 +62,18 @@ function main() {
         inject_code('window[application_name].installGumLazyHook()');
     }
 }
-main()
-    // function getMessage() {
-    //     document.addEventListener('cs2web', function(evt) {
-    //         console.log('evt', evt, evt.detail)
-    //     })
-    // }
-    // inject_code(getMessage, true)
-    // function sendMessage2Webpage(data) {
-    //     var detail = data || {}
-    //     var evt = new CustomEvent("cs2web", {
-    //         detail: detail
-    //     });
-    //     document.dispatchEvent(evt);
-    // }
-    // sendMessage2Webpage([1, 3, 5])
+main();
+// function getMessage() {
+//     document.addEventListener('cs2web', function(evt) {
+//         console.log('evt', evt, evt.detail)
+//     })
+// }
+// inject_code(getMessage, true)
+// function sendMessage2Webpage(data) {
+//     var detail = data || {}
+//     var evt = new CustomEvent("cs2web", {
+//         detail: detail
+//     });
+//     document.dispatchEvent(evt);
+// }
+// sendMessage2Webpage([1, 3, 5])
