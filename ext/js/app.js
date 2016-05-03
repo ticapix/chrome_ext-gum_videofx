6af6649d01b1375dff88ccd799fab94d89a70640
ticapix <ticapix@gmail.com>
2016-05-03 10:35:14 +0300

    do not exposer the renderer obj directly and add more state machine checks

--- ext/js/app.js
+++ ext/js/app.js
@@ -3,12 +3,12 @@ var app_init = (function() {
     var StateEnum = {
         UNINSTALLED: 0,
         LAZY_INSTALLED: 1,
-        INSTALLED: 2
+        INSTALLED: 2,
+        RUNNING: 3
     }
     var _status = StateEnum.UNINSTALLED;
     var _videofx = {}
     var _pipeline_video = document.createElement("video");
-    var _stream_video_orign = null;
     var _pipeline_renderer = null;
     var _sendMessage2ContentScript = function(data) {
         // https://gist.github.com/gordonbrander/2230317
@@ -38,35 +38,46 @@ var app_init = (function() {
         fx = 'fx_3dcube'
         return _videofx[fx].main(video);
     }
+    var _render = function(scene, camera) {
+        _pipeline_renderer.render(scene, camera);
+        debug(_status === StateEnum.RUNNING)
+        return _status === StateEnum.RUNNING;
+    }
     var _createNewStream = function(stream) {
         if (stream.getVideoTracks().length > 0) {
             debug('_pipeline_video', _pipeline_video)
             return new Promise(function(resolve, reject) {
                 var handler = function() {
-                    debug('oncanplay')
+                    debug('video canplay')
                     _pipeline_video.play();
+                    if (_status !== StateEnum.INSTALLED) {
+                        reject('webfx is not in INSTALLED state')
+                    }
+                    _status = StateEnum.RUNNING;
                     _threeRender(_pipeline_video);
                     var stream_from_effect = _pipeline_renderer.domElement.captureStream(30);
+                    if (stream_from_effect.getTracks().length != 1) {
+                        reject('too many tracks (>1) returned from captureStream(30)')
+                    }
+                    stream_from_effect.getVideoTracks()[0].addEventListener('ended', function(evt) {
+                        if (evt.target instanceof CanvasCaptureMediaStreamTrack) {
+                            debug('stopping video capture')
+                            _status = StateEnum.INSTALLED;
+                            // if canvas ended, ends the original video stream too
+                            stream_video_orign.stop()
+                        }
+                    })
                     for (var audio_track of stream.getAudioTracks()) {
                         stream_from_effect.addTrack(audio_track);
                     }
-                    stream_from_effect.getTracks().forEach(function(track) {
-                        track.addEventListener('ended', function(evt) {
-                            debug('ended', evt)
-                            if (evt.target instanceof CanvasCaptureMediaStreamTrack) {
-                                // if canvas ended, ends the original video stream too
-                                _stream_video_orign.stop()
-                            }
-                        })
-                    });
-                    debug('stream from effect', stream_from_effect.getTracks())
-                    _pipeline_video.removeEventListener('canplay', handler, false)
+                    debug('stream from effect', stream_from_effect.getTracks());
+                    _pipeline_video.removeEventListener('canplay', handler, false);
                     resolve(stream_from_effect);
                 }
                 _pipeline_video.addEventListener('canplay', handler);
                 // TODO: maybe taking the 1st video track is not optimal
-                _stream_video_orign = stream.getVideoTracks()[0]
-                var stream_to_effect = new MediaStream([_stream_video_orign]);
+                var stream_video_orign = stream.getVideoTracks()[0];
+                var stream_to_effect = new MediaStream([stream_video_orign]);
                 debug('stream to effect', stream_to_effect.getTracks());
                 _pipeline_video.src = window.URL.createObjectURL(stream_to_effect);
             })
@@ -99,7 +110,7 @@ var app_init = (function() {
                         resolve();
                         break;
                     default:
-                        reject();
+                        reject('application is not in valid state');
                 }
             }).then(function() {
                 navigator.__getUserMedia(constraints, function(stream) {
@@ -122,14 +133,15 @@ var app_init = (function() {
         _videofx[name] = module;
     }
     var self = {
-        installGumLazyHook: _installGumLazyHook,
-        installDomElt: _installDomElt,
-        defineModule: _defineModule
-    }
-    Object.defineProperty(self, "pipeline_renderer", {
-        get: function() {
-            return _pipeline_renderer;
+            installGumLazyHook: _installGumLazyHook,
+            installDomElt: _installDomElt,
+            defineModule: _defineModule,
+            render: _render
         }
-    })
+        // Object.defineProperty(self, "pipeline_renderer", {
+        //     get: function() {
+        //         return _pipeline_renderer;
+        //     }
+        // })
     return self;
 });
