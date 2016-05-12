db72fe31129586ae9ee826909c6dcf4dd0715e48
ticapix <ticapix@gmail.com>
2016-05-12 22:03:09 +0300

    add error fct

--- ext/js/util.js
+++ ext/js/util.js
@@ -1,17 +1,33 @@
 'use strict';
 
 function debug() {
-    var prefix = typeof application_name !== 'undefined' ? application_name : '-';
-    if (chrome.extension === undefined) {
-        prefix += '(web)';
-    } else {
-        prefix = chrome.runtime.getManifest().short_name;
-        if (location.protocol === 'chrome-extension:') {
-            prefix += '(bg)';
+    function _prefix() {
+        var prefix = typeof application_name !== 'undefined' ? application_name : '-';
+        if (chrome.extension === undefined) {
+            prefix += '(web)';
         } else {
-            prefix += '(cs)';
+            prefix = chrome.runtime.getManifest().short_name;
+            if (location.protocol === 'chrome-extension:') {
+                prefix += '(bg)';
+            } else {
+                prefix += '(cs)';
+            }
         }
+        return prefix;
     }
-    Array.prototype.unshift.call(arguments, prefix);
+    Array.prototype.unshift.call(arguments, _prefix());
     console.log.apply(console, arguments);
+}
+
+function error() {
+    Array.prototype.unshift.call(arguments, 'EE ');
+    debug(arguments);
+}
+
+function webfx_defineModule(name, module) {
+    if (this.videofx === undefined) {
+        debug('defining empty container for videofx on', this);
+        this.videofx = {};
+    }
+    this.videofx[name] = module(this);
 }
